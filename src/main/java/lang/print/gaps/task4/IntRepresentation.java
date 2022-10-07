9a5cbcc494bf0ac17e6ffe6a27eb1423266de5f6
Talha Keles <kelesstalha@gmail.com>
2022-10-08 02:54:41 +0300

    Done.

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 10;
+        char h = 11;
+        char a = 12;
+        char r = 13;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
