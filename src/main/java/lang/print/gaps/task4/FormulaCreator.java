bf3af9e7474106158b808e69874d829e4202b0ba
Talha Keles <kelesstalha@gmail.com>
2022-10-08 02:58:25 +0300

    Done.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -5,5 +5,6 @@ public class FormulaCreator {
         int a = 2;
         int b = 3;
         double c = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(c);
     }
 }
